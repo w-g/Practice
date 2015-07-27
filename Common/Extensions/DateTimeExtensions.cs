1a9f369aac2560d36279f9399e47f198ffd68418
Zhibin Wang <w-zb@outlook.com>
2015-07-27 18:39:16 +0800

    normal change

---
+++ Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sediment.Common
+{
+    /// <summary>
+    /// 日期的扩展
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        public static string ToFriendlyDateString(this DateTime dateTime)
+        {
+            return null;
+        }
+    }
+}
--- Practise/Event.cs
+++ Practise/Event.cs
@@ -2,67 +2,67 @@
 
 namespace Sediment.Practise
 {
-    //public class CarInfoEventArgs : EventArgs
-    //{
-    //    public CarInfoEventArgs(string car)
-    //    {
-    //        this.Car = car;
-    //    }
+    public class CarInfoEventArgs : EventArgs
+    {
+        public CarInfoEventArgs(string car)
+        {
+            this.Car = car;
+        }
 
-    //    public string Car { get; private set; }
-    //}
+        public string Car { get; private set; }
+    }
 
-    //public class CarDealer
-    //{
-    //    public event EventHandler<CarInfoEventArgs> NewCarInfo;
+    public class CarDealer
+    {
+        public event EventHandler<CarInfoEventArgs> NewCarInfo;
 
-    //    public void NewCar(string car)
-    //    {
-    //        Console.WriteLine("Car Dealer, new car {0}", car);
+        public void NewCar(string car)
+        {
+            Console.WriteLine("Car Dealer, new car {0}", car);
 
-    //        if (NewCarInfo != null)
-    //        {
-    //            NewCarInfo(this, new CarInfoEventArgs(car));
-    //        }
-    //    }
-    //}
+            if (NewCarInfo != null)
+            {
+                NewCarInfo(this, new CarInfoEventArgs(car));
+            }
+        }
+    }
 
-    //public class Consumer
-    //{
-    //    private string name;
+    public class Consumer
+    {
+        private string name;
 
-    //    public Consumer(string name)
-    //    {
-    //        this.name = name;
-    //    }
+        public Consumer(string name)
+        {
+            this.name = name;
+        }
 
-    //    public void NewCarIsHere(object sender, CarInfoEventArgs eventArgs)
-    //    {
-    //        Console.WriteLine("{0}: car {1} is new", this.name, eventArgs.Car);
-    //    }
-    //}
+        public void NewCarIsHere(object sender, CarInfoEventArgs eventArgs)
+        {
+            Console.WriteLine("{0}: car {1} is new", this.name, eventArgs.Car);
+        }
+    }
 
-    //class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        var dealer = new CarDealer();
+    class EventProgram
+    {
+        internal static void Run()
+        {
+            var dealer = new CarDealer();
 
-    //        var lucy = new Consumer("lucy");
-    //        dealer.NewCarInfo += lucy.NewCarIsHere; // 订阅事件
+            var lucy = new Consumer("lucy");
+            dealer.NewCarInfo += lucy.NewCarIsHere; // 订阅事件
 
-    //        dealer.NewCar("Mercedes"); // 发布事件
+            dealer.NewCar("Mercedes"); // 发布事件
 
-    //        var lily = new Consumer("lily");
-    //        dealer.NewCarInfo += lily.NewCarIsHere; // 订阅事件
+            var lily = new Consumer("lily");
+            dealer.NewCarInfo += lily.NewCarIsHere; // 订阅事件
 
-    //        dealer.NewCar("Ferrari"); // 发布事件
+            dealer.NewCar("Ferrari"); // 发布事件
 
-    //        dealer.NewCarInfo -= lucy.NewCarIsHere; // 取消订阅
+            dealer.NewCarInfo -= lucy.NewCarIsHere; // 取消订阅
 
-    //        dealer.NewCar("Toyota");
+            dealer.NewCar("Toyota");
 
-    //        Console.ReadKey();
-    //    }
-    //}
+            Console.ReadKey();
+        }
+    }
 }
