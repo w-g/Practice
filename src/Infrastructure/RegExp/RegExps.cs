e8b646cb348dc74a99ca750bfb99c4a25642800b
zb.wang <w-zb@outlook.com>
2017-06-08 13:49:47 +0800

    add Infrastracture.RegExp

---
+++ src/Infrastructure/RegExp/RegExps.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Sediment.RegExp
+{
+    /// <summary>
+    /// 正则表达式实例
+    /// </summary>
+    public class RegExps
+    {
+        /// <summary>
+        /// 提取img标签的src属性值
+        /// </summary>
+        public const string IMG_SRC = "(?<=\\<img[^\\>]+src=\")[^\"]+";
+
+
+    }
+}
